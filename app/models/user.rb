2dc9beddbc110181b73e62a33570e5e8a0d839a4
nicolo.ereni <nicolo.ereni@nozominetworks.com>
2023-02-08 17:04:10 +0100

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
+  validates :email, presence: true, length: {maximum: 105}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_073737) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_082307) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_073737) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
