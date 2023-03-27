90c8c53173f836c4f884de7d105413bba63498cc
Nicolò <nicolo.ereni@nozominetworks.com>
2023-03-27 16:30:32 +0200

    categories article category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   belongs_to :user
 
   def username
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  validates :name, presence: true
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
